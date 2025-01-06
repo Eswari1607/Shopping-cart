029e914630719d7d3532ce41bff977f2755d7682
Eswari1607 <eswarikamala1607@gmail.com>
2025-01-06 15:43:38 +0530

    Installed materialui dependencies

--- ShoppingCart/vite.config.js
+++ ShoppingCart/vite.config.js
@@ -1,19 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/React_Tasks/',
   plugins: [react()],
-  build: { 
-    outDir: 'dist',
-    rollupOptions: {
-      external: ['@mui/material']
-    }
-  },
-  resolve: { 
-    alias: { 
-      '@': '/src' 
-    } 
-  }
-})
+});
